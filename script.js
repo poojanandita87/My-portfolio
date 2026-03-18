9e2af90412581c7f7c084adcaf09e7ebd7b1a776
poojanandita87 <poojanandita87gmail.com>
2026-03-18 17:43:32 +0530

    update portfolio

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ body:JSON.stringify({name,email,message})
 alert("Message sent!");
 
 });
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = document.querySelector("[name='name']").value;
+  const email = document.querySelector("[name='email']").value;
+  const message = document.querySelector("[name='message']").value;
+
+  fetch("http://localhost:3000/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name, email, message })
+  })
+  .then(res => res.text())
+  .then(data => {
+    alert("✅ " + data);
+  })
+  .catch(err => {
+    alert("❌ Error");
+  });
+});
