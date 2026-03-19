838834fe5c74cb73959b96094b1bf508d3a002fc
poojanandita87 <poojanandita87gmail.com>
2026-03-19 17:56:43 +0530

    update portfolio

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
+document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const message = document.querySelector("#message").value;
 
-  fetch ( "https://my-portfolio-y5p2.onrender.com/contact", {
+  const res = await fetch("http://localhost:5000/contact", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ name, email, message })
-  })
-  .then(res => res.text())
-  .then(data => {
-    alert("✅ " + data);
-  })
-  .catch(err => {
-    alert("❌ Error");
   });
+
+  alert("Message sent!");
 });
